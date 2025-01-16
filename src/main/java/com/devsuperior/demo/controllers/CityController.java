ea7735241bf596e389483f679a76a05ad93aa5ef
Andre Zicatti <andrezicatti@gmail.com>
2025-01-16 11:50:52 -0300

    :sparkles: feat: City and Event Controllers (GET all and POST implemented)

---
+++ src/main/java/com/devsuperior/demo/controllers/CityController.java
@@ -0,0 +1,42 @@
+package com.devsuperior.demo.controllers;
+
+import com.devsuperior.demo.dto.CityDTO;
+import com.devsuperior.demo.services.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/cities")
+public class CityController {
+
+    @Autowired
+    private CityService service;
+
+    @GetMapping
+    public ResponseEntity<List<CityDTO>> findAll() {
+        return ResponseEntity
+                .ok()
+                .body(service.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<CityDTO> insert(@RequestBody CityDTO dto) {
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(dto.getId())
+                .toUri();
+        return ResponseEntity
+                .created(uri)
+                .body(service.insert(dto));
+    }
+}
